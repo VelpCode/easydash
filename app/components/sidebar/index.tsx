8694de4f70b72d5742ef8092ac8c9d2ac065223e
velpcode <velpmail@gmail.com>
2024-05-14 16:24:51 -0400

    easydash

---
+++ app/components/sidebar/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='min-h-screen border-r w-64'>
+        <div></div>
+    </div>
+  )
+}
+
+export default Sidebar
