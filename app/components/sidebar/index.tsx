c1e49c2b54725e73fdf312ce53a4e6408d7640b0
velpcode <velpmail@gmail.com>
2024-05-15 15:00:59 -0400

    changesmade

--- app/components/sidebar/index.tsx
+++ app/components/sidebar/index.tsx
@@ -42,21 +42,21 @@ const Sidebar = () => {
 
   return (
     <div className='fixed left-0 top-0 min-h-screen w-64 z-10 p-10 bg-[#F4F5F5] shadow-md'>
-      <div className='align-middle flex gap-2'>
+      <div className='align-middle flex gap-2 mb-10'>
         <Image src = "/aman.jpeg" width={80} height={80} alt = 'pic' className='rounded-2xl shadow-md'></Image>
         <div>
           <h1 className = "text-xs mt-2 text-neutral-500 font-semibold">Aman Velpula</h1>
           <p className='text-xs text-neutral-400'>aman@gmail.com</p>
+          <div className = "flex gap-3 mt-2">
+            <XIcon className='text-neutral-400 text-md'/>
+            <YoutubeIcon className = 'text-neutral-400 text-md  '/> 
+            <InstagramIcon className = 'text-neutral-400 text-md'/>
         </div>
-      </div>
-      <div className = "mt-7 flex gap-3 items-center align-middle mb-10">
-        <XIcon className='text-neutral-400 text-4xl '/>
-        <YoutubeIcon className = 'text-neutral-400 text-4xl'/> 
-        <InstagramIcon className = 'text-neutral-400 text-4xl'/>
+        </div>
       </div>
 
       {Items.map((item, index) => (
-        <div key={index} className='text-neutral-500 p-2 flex gap-4 mt-3 text-sm items-center cursor-pointer hover:bg-neutral-200 w-full rounded'>
+        <div key={index} className='text-neutral-400 p-2 flex gap-4 mt-3 text-sm items-center cursor-pointer hover:bg-neutral-200 w-full rounded-xl'>
           {item.icon}
           {item.name}
         </div>
