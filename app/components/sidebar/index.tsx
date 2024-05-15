f1c4d6aced68081a0fe803b3a5383ed62d31f37a
velpcode <velpmail@gmail.com>
2024-05-15 14:56:05 -0400

    Build Port

--- app/components/sidebar/index.tsx
+++ app/components/sidebar/index.tsx
@@ -3,8 +3,43 @@ import Image from 'next/image'
 import XIcon from '@mui/icons-material/X';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import YoutubeIcon from "@mui/icons-material/YouTube"
+import { Home } from 'lucide-react';
+import { Build } from '@mui/icons-material';
+import { PaintBucket } from 'lucide-react';
+import { Store } from 'lucide-react';
+import { Pen } from 'lucide-react';
 
 const Sidebar = () => {
+
+
+  const Items = [
+
+    {
+      icon: <Home />,
+      name: "Home"
+    },
+
+    {
+      icon: <Build />,
+      name: "Projects"
+    },
+    {
+      icon: <PaintBucket />,
+      name: "Passions"
+    },
+    {
+      icon: <Store />,
+      name: "Store"
+    },
+    {
+      icon: <Pen />,
+      name: "Blog"
+    }
+
+
+  ]
+
+
   return (
     <div className='fixed left-0 top-0 min-h-screen w-64 z-10 p-10 bg-[#F4F5F5] shadow-md'>
       <div className='align-middle flex gap-2'>
@@ -14,11 +49,19 @@ const Sidebar = () => {
           <p className='text-xs text-neutral-400'>aman@gmail.com</p>
         </div>
       </div>
-      <div className = "mt-7 flex gap-3 items-center align-middle">
+      <div className = "mt-7 flex gap-3 items-center align-middle mb-10">
         <XIcon className='text-neutral-400 text-4xl '/>
         <YoutubeIcon className = 'text-neutral-400 text-4xl'/> 
         <InstagramIcon className = 'text-neutral-400 text-4xl'/>
       </div>
+
+      {Items.map((item, index) => (
+        <div key={index} className='text-neutral-500 p-2 flex gap-4 mt-3 text-sm items-center cursor-pointer hover:bg-neutral-200 w-full rounded'>
+          {item.icon}
+          {item.name}
+        </div>
+      ))}
+
     </div>
   )
 }
