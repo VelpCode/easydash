6633bfa3c4bcb27f54a27caa171b57c693664e48
velpcode <velpmail@gmail.com>
2024-05-30 20:49:06 -0400

    submewet

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -11,50 +11,39 @@ export default function Page() {
             <Web />
             <div className="text-6xl font-semibold text-neutral-800 max-w-[600px] mb-2">My Shop</div>
             <h1 className='text-3xl font-medium mb-8 text-gray-500'>Some of the things I've dabbled with..</h1>
-            <div>
-                <div className="flex gap-10 mt-[100px]">
-                    <div className="left rounded-lg">
-                    <Image src = "/Mockup.png" alt = "mockup" height={400} width={400} className="rounded-md shadow-xl"/>
+
+            <div className="grid grid-cols-2 md:grid-cols-2 p-20 gap-2">
+                <div className="rounded">
+                    <div className=" rounded-md">
+                        <Image src = "/Mockup.png" alt="Height" height={300} width={300} />
                     </div>
-                    <div className="right">
-                    <div className="text-3xl font-medium text-neutral-600">Project One</div>
-                    <p className="mt-5 text-sm text-neutral-400 max-w-[400px] mb-10 itali">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lorem massa, lacinia eu risus ac, tempor semper nibh. Integer volutpat enim sed feugiat semper. Quisque elementum diam quis ornare suscipit. Nunc tristique nunc nec leo fermentum pharetra. Duis gravida, nisl vitae rhoncus suscipit, dui turpis viverra purus, in maximus ex ante eu tortor. Mauris semper ut elit ac aliquam. Donec sed dolor laoreet, dignissim turpis sed, posuere dolor.
-                    Donec nibh turpis, ultrices eget purus in, mollis fermentum libero. <br /> <br /> lacinia eu risus ac, tempor semper nibh. Integer volutpat enim sed feugiat
-                    </p>
-                    <div className="flex gap-2">
-                        <button className="px-6 py-2 bg-neutral-800 text-white rounded-lg shadow-md font-medium transform hover:-translate-y-1 transition duration-400">
-                            Preview
-                        </button>
-                        <button className="px-6 py-2 bg-white text-black border shadow-md rounded-lg font-medium transform hover:-translate-y-1 transition duration-400">
-                            GitHub
-                        </button>
-                    </div>
-    
+                    <div className="bg-neutral-200 p-5">
+
                     </div>
                 </div>
-                <div className="flex gap-10 mt-[100px]">
-
-                    <div className="left border rounded h-[400px] w-[400px]">
-                        <Image src = "/Reflex.png" alt = "reflex" height={400} width={400} className="rounded-md shadow-xl"/>
+                
+                <div className="rounded-xl h-[500px] w-[500px]">
+                    <div className="bg-white">
+                        <h1>hi</h1>
                     </div>
-
-                    <div className="right">
-                        <h1><div className="right">
-                    <div className="text-3xl font-medium text-neutral-600">Project Two</div>
-                    <p className="mt-5 text-sm text-neutral-400 max-w-[400px] mb-10 itali">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lorem massa, lacinia eu risus ac, tempor semper nibh. Integer volutpat enim sed feugiat semper. Quisque elementum diam quis ornare suscipit. Nunc tristique nunc nec leo fermentum pharetra. Duis gravida, nisl vitae rhoncus suscipit, dui turpis viverra purus, in maximus ex ante eu tortor. Mauris semper ut elit ac aliquam. Donec sed dolor laoreet, dignissim turpis sed, posuere dolor.
-                    Donec nibh turpis, ultrices eget purus in, mollis fermentum libero. <br /> <br /> lacinia eu risus ac, tempor semper nibh. Integer volutpat enim sed feugiat
-                    </p>
-                    <div className="flex gap-2">
-                        <button className="px-6 py-2 bg-neutral-800 text-white rounded-lg shadow-md font-medium transform hover:-translate-y-1 transition duration-400">
-                            Preview
-                        </button>
-                        <button className="px-6 py-2 bg-white text-black border shadow-md rounded-lg font-medium transform hover:-translate-y-1 transition duration-400">
-                            GitHub
-                        </button>
+                    <div className="bg-neutral-200 p-5">
+                        <h1>hi</h1>
                     </div>
-                    </div></h1>
+                </div>
+                <div className="rounded-xl h-[500px] w-[500px]">
+                    <div className="bg-white">
+                        <h1>hi</h1>
+                    </div>
+                    <div className="bg-neutral-300 p-5">
+                        <h1>hi</h1>
+                    </div>
+                </div>
+                <div className="rounded-xl h-[500px] w-[500px]">
+                    <div className="bg-white">
+                        <h1>hi</h1>
+                    </div>
+                    <div className="bg-neutral-300 p-5">
+                        <h1>hi</h1>
                     </div>
                 </div>
             </div>
