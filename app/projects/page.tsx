57e915924e0452f85a27204894f1141d8f20b400
velpcode <velpmail@gmail.com>
2024-06-04 17:18:35 -0400

    Responsiv

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -12,13 +12,13 @@ const Projects = () => {
             <div className="text-6xl font-semibold text-neutral-800 max-w-[600px] mb-2">My Projects</div>
             <h1 className='text-3xl font-medium mb-8 text-gray-500'>Some of the things I've dabbled with..</h1>
             <div className="mb-10">
-                <div className="flex gap-10 mt-[100px]">
+                <div className="flex gap-10 mt-[100px] md:flex-row sm:flex-col xs:flex-col ">
                     <div className="left rounded-lg">
                     <Image src = "/Mockup.png" alt = "mockup" height={400} width={400} className="rounded-md shadow-xl"/>
                     </div>
                     <div className="right">
-                    <div className="text-3xl font-medium text-neutral-600">Project One</div>
-                    <p className="mt-5 text-sm text-neutral-400 max-w-[400px] mb-10 itali">
+                    <div className="text-3xl font-medium text-neutral-600 lg:text-3xl md:text-xl sm:text-md">Project One</div>
+                    <p className="mt-5 text-sm text-neutral-400 max-w-[400px] mb-10 lg:text-sm md:text-xs sm:text-xs">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lorem massa, lacinia eu risus ac, tempor semper nibh. Integer volutpat enim sed feugiat semper. Quisque elementum diam quis ornare suscipit. Nunc tristique nunc nec leo fermentum pharetra. Duis gravida, nisl vitae rhoncus suscipit, dui turpis viverra purus, in maximus ex ante eu tortor. Mauris semper ut elit ac aliquam. Donec sed dolor laoreet, dignissim turpis sed, posuere dolor.
 
 Donec nibh turpis, ultrices eget purus in, mollis fermentum libero. <br /> <br /> lacinia eu risus ac, tempor semper nibh. Integer volutpat enim sed feugiat
@@ -34,7 +34,7 @@ Donec nibh turpis, ultrices eget purus in, mollis fermentum libero. <br /> <br /
     
                     </div>
                 </div>
-                <div className="flex gap-10 mt-[100px]">
+                <div className="flex gap-10 mt-[100px] md:flex-row sm:flex-col">
 
                     <div className="left border rounded h-[400px] w-[400px]">
                         <Image src = "/Reflex.png" alt = "reflex" height={400} width={400} className="rounded-md shadow-xl"/>
@@ -43,7 +43,7 @@ Donec nibh turpis, ultrices eget purus in, mollis fermentum libero. <br /> <br /
                     <div className="right">
                         <h1><div className="right">
                     <div className="text-3xl font-medium text-neutral-600">Project Two</div>
-                    <p className="mt-5 text-sm text-neutral-400 max-w-[400px] mb-10 itali">
+                    <p className="mt-5 text-sm text-neutral-400 max-w-[400px] mb-10 lg:text-sm md:text-xs sm:text-xs">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lorem massa, lacinia eu risus ac, tempor semper nibh. Integer volutpat enim sed feugiat semper. Quisque elementum diam quis ornare suscipit. Nunc tristique nunc nec leo fermentum pharetra. Duis gravida, nisl vitae rhoncus suscipit, dui turpis viverra purus, in maximus ex ante eu tortor. Mauris semper ut elit ac aliquam. Donec sed dolor laoreet, dignissim turpis sed, posuere dolor.
 
 Donec nibh turpis, ultrices eget purus in, mollis fermentum libero. <br /> <br /> lacinia eu risus ac, tempor semper nibh. Integer volutpat enim sed feugiat
--- app/store/page.tsx
+++ app/store/page.tsx
@@ -12,38 +12,38 @@ export default function Page() {
             <div className="text-6xl font-semibold text-neutral-800 max-w-[600px] mb-2">My Shop</div>
             <h1 className='text-3xl font-medium mb-8 text-gray-500'>Some of the things I've dabbled with..</h1>
 
-            <div className="grid grid-cols-2 md:grid-cols-2 p-20 gap-2">
+            <div className="grid grid-cols-2 md:grid-cols-2 gap-5">
                 <div className="rounded">
                     <div className=" rounded-md">
-                        <Image src = "/Mockup.png" alt="Height" height={300} width={300} />
+                        <Image src = "/Mockup.png" alt="Height" height={400} width={400} className="rounded-lg"/>
                     </div>
-                    <div className="bg-neutral-200 p-5">
+                    <div className="bg-neutral-200 p-5 w-[400px]">
 
                     </div>
                 </div>
                 
-                <div className="rounded-xl h-[500px] w-[500px]">
-                    <div className="bg-white">
-                        <h1>hi</h1>
+                <div className="rounded">
+                    <div className=" rounded-md">
+                        <Image src = "/Mockup.png" alt="Height" height={400} width={400} className="rounded-lg"/>
                     </div>
-                    <div className="bg-neutral-200 p-5">
-                        <h1>hi</h1>
-                    </div>
-                </div>
-                <div className="rounded-xl h-[500px] w-[500px]">
-                    <div className="bg-white">
-                        <h1>hi</h1>
-                    </div>
-                    <div className="bg-neutral-300 p-5">
-                        <h1>hi</h1>
+                    <div className="bg-neutral-200 p-5 w-[400px]">
+
                     </div>
                 </div>
-                <div className="rounded-xl h-[500px] w-[500px]">
-                    <div className="bg-white">
-                        <h1>hi</h1>
+                <div className="rounded">
+                    <div className=" rounded-md">
+                        <Image src = "/Mockup.png" alt="Height" height={400} width={400} className="rounded-lg"/>
                     </div>
-                    <div className="bg-neutral-300 p-5">
-                        <h1>hi</h1>
+                    <div className="bg-neutral-200 p-5 w-[400px]">
+
+                    </div>
+                </div>
+                <div className="rounded">
+                    <div className=" rounded-md">
+                        <Image src = "/Mockup.png" alt="Height" height={400} width={400} className="rounded-lg"/>
+                    </div>
+                    <div className="bg-neutral-200 p-5 w-[400px]">
+
                     </div>
                 </div>
             </div>
